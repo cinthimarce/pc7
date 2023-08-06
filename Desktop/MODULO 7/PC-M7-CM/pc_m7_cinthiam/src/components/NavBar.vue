68cb20ee3f164785492e814125278a5251a76853
cinthia muñoz <chileabstracto@gmail.com>
2023-08-06 16:59:57 -0400

    empezando el el Homeview

---
+++ Desktop/MODULO 7/PC-M7-CM/pc_m7_cinthiam/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-app-bar color="deep-purple accent-4" dense dark>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Cursos AlfaWeb</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-home</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-wrench</v-icon>
+            </v-btn>
+
+            
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'navbar-view',
+    // props: {},
+    data: function () {
+        return {}
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped></style>
